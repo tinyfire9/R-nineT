f810f51c425d79ae7134440d7dcf358d48853733
slimfire <personalprojectyohannes@gmail.com>
2020-07-15 18:22:17 -0400

    [TASK] initial commit

---
+++ src/test/java/com/App/RnineT/RnineTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.RnineT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnineTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
