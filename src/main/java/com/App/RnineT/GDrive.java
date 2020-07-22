19d9a6976c04dabf6998f13bc1670a9ac5fc578f
slimfire <personalprojectyohannes@gmail.com>
2020-07-22 09:27:49 -0400

    [TASK] remove getSubDirectoryIDs

--- src/main/java/com/App/RnineT/GDrive.java
+++ src/main/java/com/App/RnineT/GDrive.java
@@ -10,7 +10,6 @@ import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class GDrive implements RnineTDrive {
@@ -45,58 +44,41 @@ public class GDrive implements RnineTDrive {
         }
     }
 
-    private List<String> getSubDirectoryIDs(String directoryID){
-        List<String> subDirectoryIDs = new ArrayList<String>();
-
-        try{
-            List<File> subDirectories = driveClient.files().list()
-                    .setQ(String.format("'%s' in parents", directoryID))
-                    .execute()
-                    .getFiles();
-
-            for(int i = 0; i < subDirectories.size(); i++){
-                subDirectoryIDs.add(subDirectories.get(i).getId());
-            }
-
-            return subDirectoryIDs;
-        } catch (Exception e){
-            System.out.println("Error fetching subdirectory IDs for directoryID: " + directoryID);
-            System.out.println(e.getMessage());
-        }
-
-        return subDirectoryIDs;
-    }
-
-    public boolean download(String token, List<String> directoryIDs, String downloadDirectoryPath){
+    public boolean download(String token, String directoryID, String downloadDirectoryPath){
         try {
             Directory directory = Directory.getDirectoryInstance();
             Drive driveClient = getDriveClient(token);
             Drive.Files files = driveClient.files();
 
-            for(int i = 0; i < directoryIDs.size(); i++){
-                String directoryID = directoryIDs.get(i);
-                File file = files.get(directoryID).execute();
-                String subDirectoryDownloadPath = downloadDirectoryPath + "/" + file.getName();
+            File file = files.get(directoryID).execute();
+            String subDirectoryDownloadPath = downloadDirectoryPath + "/" + file.getName();
 
-                String mimeType = file.getMimeType();
-                if(file.getMimeType().contains("folder")){
-                    directory.makeDir(subDirectoryDownloadPath);
+            String mimeType = file.getMimeType();
+            if(mimeType.contains("folder")){
+                directory.makeDir(subDirectoryDownloadPath);
 
-                    this.download(token, this.getSubDirectoryIDs(directoryID), subDirectoryDownloadPath);
-                } else if(mimeType.contains("google-apps")){
-                    continue;
-                }else {
-                    FileOutputStream outputStream = new FileOutputStream(subDirectoryDownloadPath);
-                    driveClient
+                List<File> subdirectories = driveClient
+                        .files()
+                        .list()
+                        .setQ(String.format("'%s' in parents", directoryID))
+                        .execute()
+                        .getFiles();
+
+                for(int i = 0; i < subdirectories.size(); i++){
+                    this.download(token, subdirectories.get(i).getId(), subDirectoryDownloadPath);
+                }
+
+            } else if(!mimeType.contains("google-apps")) {
+                FileOutputStream outputStream = new FileOutputStream(subDirectoryDownloadPath);
+                driveClient
                         .files()
                         .get(directoryID)
                         .executeMediaAndDownloadTo(outputStream);
-                }
             }
 
             return true;
         } catch (Exception e){
-            System.out.println("Error downloading directories: " + directoryIDs.toString());
+            System.out.println("Error downloading directories: " + directoryID);
             System.out.println(e);
 
             return false;
