701580b92749d34115e817357d768c20974dcaef
slimfire <personalprojectyohannes@gmail.com>
2020-07-28 12:53:09 -0400

    [TASK] Add getter methods on jobId, drive and token. Overload constructor.

--- src/main/java/com/App/RnineT/RnineTDrive.java
+++ src/main/java/com/App/RnineT/RnineTDrive.java
@@ -1,17 +1,37 @@
 package com.App.RnineT;
 
-import com.google.api.services.drive.Drive;
-
-import java.util.List;
+import java.util.UUID;
 
 abstract public class RnineTDrive<DriveClient> {
-    protected String token;
-    protected String jobID;
-    protected DriveClient drive;
+    private String token;
+    private String jobID;
+    private DriveClient drive;
+
+    public RnineTDrive(String token){
+        this.token = token;
+        this.jobID = UUID.randomUUID().toString();
+        this.drive = this.initDriveClient();
+
+        Directory directory  = Directory.getDirectoryInstance();
+        directory.makeDirByJobID(jobID);
+    }
+
     public RnineTDrive(String token, String jobID){
         this.token = token;
         this.jobID = jobID;
         this.drive = this.initDriveClient();
+    }
+
+    public String getJobID(){
+        return jobID;
+    }
+
+    public DriveClient getDrive(){
+        return drive;
+    }
+
+    public String getToken(){
+        return token;
     }
 
     abstract protected DriveClient initDriveClient();
