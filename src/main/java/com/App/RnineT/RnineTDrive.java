a43ee3235310236344dd736e8af08967d661a096
slimfire <personalprojectyohannes@gmail.com>
2020-07-27 22:03:58 -0400

    [TASK] set token and jobId on constructor, add initDriveClient abstract method

--- src/main/java/com/App/RnineT/RnineTDrive.java
+++ src/main/java/com/App/RnineT/RnineTDrive.java
@@ -2,7 +2,17 @@ package com.App.RnineT;
 
 import java.util.List;
 
-public interface RnineTDrive {
-    public boolean download(String token, String directoryID, String downloadDirectoryPath);
-    public boolean upload(String token, String directoryPath, String directoryName, String uploadDirectoryID);
+abstract public class RnineTDrive<DriveClient> {
+    protected String token;
+    protected String jobID;
+    protected DriveClient drive;
+    public RnineTDrive(String token, String jobID){
+        this.token = token;
+        this.jobID = jobID;
+        this.initDriveClient();
+    }
+
+    abstract protected void initDriveClient();
+    abstract public boolean download(String directoryID, String downloadDirectoryPath);
+    abstract public boolean upload(String directoryPath, String directoryName, String uploadDirectoryID);
 }
