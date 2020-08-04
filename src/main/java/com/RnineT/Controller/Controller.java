2dc144d3afa2ea0c5b8317df78faf0ff8222017a
slimfire <personalprojectyohannes@gmail.com>
2020-08-04 19:25:41 -0400

    [FEATURE] save job info on db

--- src/main/java/com/RnineT/Controller/Controller.java
+++ src/main/java/com/RnineT/Controller/Controller.java
@@ -1,26 +1,33 @@
 package com.RnineT.Controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.RnineT.Status.Db.Jobs.Job;
+import com.RnineT.Status.Db.Jobs.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 
-@SpringBootApplication
+@org.springframework.stereotype.Controller
 @RestController
 public class Controller {
-	public static void main(String[] args) {
-		SpringApplication.run(Controller.class, args);
-	}
+    @Autowired
+    private JobRepository jobRepository;
 
 	@GetMapping("/")
 	public String index() {
 		return "Hello world";
 	}
 
-	@PostMapping(value = "/transfer", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(path = "/transfer", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
 	public String transfer(@RequestBody TransferRequest request){
 		RnineT rnineT = new RnineT(request);
+
+		Job job = new Job();
+		job.setSource(((TransferRequest.SourceDrive )request.getSourceDrive()).getName());
+		job.setDest(((TransferRequest.DestDrive )request.getDestDrive()).getName());
+		job.setId(rnineT.getJobID());
+
+		jobRepository.save(job);
 
 		rnineT.startTransfer();
 
