3f608bd560d701442132549d941e2239ab3a0c84
slimfire <personalprojectyohannes@gmail.com>
2020-08-04 19:27:12 -0400

    [TASK] Add download repository

---
+++ src/main/java/com/RnineT/Status/Db/Downloads/DownloadRepository.java
@@ -0,0 +1,5 @@
+package com.RnineT.Status.Db.Downloads;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DownloadRepository extends CrudRepository<Download, String> {}
