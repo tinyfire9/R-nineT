29816a85d671ef366fa54f472b4e5f8ee8c9dbdc
slimfire <personalprojectyohannes@gmail.com>
2020-08-10 18:30:46 -0400

    [FEATURE] Add directory repo

---
+++ src/main/java/com/RnineT/Status/Db/Directories/DirectoryRepository.java
@@ -0,0 +1,6 @@
+package com.RnineT.Status.Db.Directories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DirectoryRepository extends CrudRepository<Directory, String> {}
