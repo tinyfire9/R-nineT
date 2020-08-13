33999657e21fb6e85db965624187e2a22bbc3e16
slimfire <personalprojectyohannes@gmail.com>
2020-08-12 20:26:34 -0400

    [FEATURE] RNINET-12 #start-progress Add status, totalItemsCount & size columns

--- src/main/java/com/RnineT/Status/Database/Jobs/Job.java
+++ src/main/java/com/RnineT/Status/Database/Jobs/Job.java
@@ -9,6 +9,23 @@ public class Job {
     private String id;
     private String source;
     private String dest;
+    private String status;
+    private Long totalItemsCount;
+    private Long size;
+
+    public static final class STATES {
+        public static final String INITIATED = "INITIATED";
+        public static final String TRANSFERRING = "TRANSFERRING";
+        public static final String COMPLETED = "COMPLETED";
+    }
+
+    public Long getTotalItemsCount() {
+        return totalItemsCount;
+    }
+
+    public void setTotalItemsCount(Long totalItemsCount) {
+        this.totalItemsCount = totalItemsCount;
+    }
 
     public String getId() {
         return id;
@@ -20,6 +37,22 @@ public class Job {
 
     public String getDest() {
         return dest;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
     }
 
     public void setId(String id) {
