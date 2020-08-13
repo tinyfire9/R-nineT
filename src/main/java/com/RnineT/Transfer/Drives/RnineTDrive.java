11913ae07ddca75d1dc30d16cb4e7a5cb2fae216
slimfire <personalprojectyohannes@gmail.com>
2020-08-12 20:44:21 -0400

    RNINET-12 [FEATURE] add getTotalItemsCount and getTotalSizeInBytes on RnineTDrive

--- src/main/java/com/RnineT/Transfer/Drives/RnineTDrive.java
+++ src/main/java/com/RnineT/Transfer/Drives/RnineTDrive.java
@@ -3,9 +3,11 @@ package com.RnineT.Transfer.Drives;
 import com.RnineT.Transfer.Transfer.*;
 import com.RnineT.Transfer.Storage.Directory;
 
-import java.util.UUID;
+import java.util.*;
 
 abstract public class RnineTDrive<DriveClient> {
+    private Long totalItemsCount;
+    private Long totalSizeInBytes;
     private String token;
     private String jobID;
     private DriveClient drive;
@@ -37,7 +39,38 @@ abstract public class RnineTDrive<DriveClient> {
         return token;
     }
 
+    public Long getTotalItemsCount(ArrayList<String> selectedItems) {
+        if(this.totalItemsCount == null){
+            Map<String, Long> sizeAndItemsCount =  new HashMap<String, Long>();
+            sizeAndItemsCount.put("totalSizeInBytes", (long) 0);
+            sizeAndItemsCount.put("totalItemsCount", (long) 0);
+
+            this.fetchTotalSizeInBytesAndTotalItemsCount(selectedItems, sizeAndItemsCount);
+
+            this.totalSizeInBytes = sizeAndItemsCount.get("totalSizeInBytes");
+            this.totalItemsCount = sizeAndItemsCount.get("totalItemsCount");
+        }
+        return this.totalItemsCount;
+    }
+
+    public Long getTotalSizeInBytes(ArrayList<String> selectedItems){
+        if(this.totalSizeInBytes == null){
+            Map<String, Long> sizeAndItemsCount = new HashMap<String, Long>();
+            sizeAndItemsCount.put("totalSizeInBytes", (long) 0);
+            sizeAndItemsCount.put("totalItemsCount", (long) 0);
+
+            this.fetchTotalSizeInBytesAndTotalItemsCount(selectedItems, sizeAndItemsCount);
+
+            System.out.println(sizeAndItemsCount.toString());
+
+            this.totalSizeInBytes = sizeAndItemsCount.get("totalSizeInBytes");
+            this.totalItemsCount = sizeAndItemsCount.get("totalItemsCount");
+        }
+        return this.totalSizeInBytes;
+    }
+
     abstract protected DriveClient initDriveClient();
+    abstract protected void fetchTotalSizeInBytesAndTotalItemsCount(ArrayList<String> selectedItems, Map<String, Long> output);
     abstract public boolean download(String directoryID, String downloadDirectoryPath, Callback callback);
     abstract public boolean upload(String localDirectoryID, String directoryPath, String directoryName, String uploadDirectoryID, Callback callback);
 }
