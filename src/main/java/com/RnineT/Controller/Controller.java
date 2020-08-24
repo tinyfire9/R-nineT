b5bb7ffc2730a320aee0b46a121df156438c95c1
slimfire <personalprojectyohannes@gmail.com>
2020-08-24 13:29:50 -0400

    [FEATURE] add statusd on transfer request

--- src/main/java/com/RnineT/Controller/Controller.java
+++ src/main/java/com/RnineT/Controller/Controller.java
@@ -4,11 +4,14 @@ import com.RnineT.Status.Database.Directories.DirectoryRepository;
 import com.RnineT.Status.Database.Jobs.Job;
 import com.RnineT.Status.Database.Jobs.JobRepository;
 import com.RnineT.Status.Status;
+import com.RnineT.Status.Statusd;
 import com.RnineT.Transfer.Storage.Storage;
 import com.RnineT.Transfer.Transfer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 
 @org.springframework.stereotype.Controller
@@ -28,6 +31,12 @@ public class Controller {
 	@CrossOrigin(value = "https://localhost:3000")
 	@PostMapping(path = "/transfer", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
 	public String transfer(@RequestBody TransferRequest request){
+		Statusd statusd = new Statusd(jobRepository, directoryRepository);
+		CompletableFuture
+			.runAsync(() -> {
+				statusd.start();
+			});
+
 		Transfer transfer = new Transfer(request, jobRepository, directoryRepository);
 		Status status = new Status(jobRepository, directoryRepository);
 		Storage storage = Storage.getStorageInstance();
