379e97cdbaf586a251843e092276ad2c3b8cf3fb
slimfire <personalprojectyohannes@gmail.com>
2020-08-26 23:00:15 -0400

    RNINET-37 [FEATURE] add download method on Dropbox

---
+++ src/main/java/com/RnineT/Transfer/Drives/Dropbox/Dropbox.java
@@ -0,0 +1,133 @@
+package com.RnineT.Transfer.Drives.Dropbox;
+
+import com.RnineT.Transfer.Drives.RnineTDrive;
+import com.RnineT.Transfer.Response;
+import com.RnineT.Transfer.Storage.Directory;
+import com.RnineT.Transfer.Transfer;
+import com.dropbox.core.DbxDownloader;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public class Dropbox extends RnineTDrive<DbxClientV2> {
+    private final String ITEM_TYPE_FILE = "file";
+    private final String ITEM_TYPE_FOLDER = "folder";
+
+    public Dropbox(String token){
+        super(token);
+    }
+
+    public Dropbox(String token, String jobID){
+        super(token, jobID);
+    }
+
+    @Override
+    protected DbxClientV2 initDriveClient() {
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("R nineT").build();
+        DbxClientV2 client = new DbxClientV2(config, getToken());
+
+        return client;
+    }
+
+    private Map<String, String> convertMetadataToMap(Metadata metadata){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> fileMetadataMap = mapper.readValue(metadata.toString(), new TypeReference<Map<String, String>>() {});
+
+            return fileMetadataMap;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public boolean download(String directoryPath, String downloadDirectoryPath, Transfer.Callback callback) {
+        Directory directory = Directory.getDirectoryInstance();
+        directoryPath = directoryPath.equals("root") ? "" : directoryPath;
+        try{
+            DbxUserFilesRequests files = getDrive().files();
+            Metadata metadata = files.getMetadata(directoryPath);
+            Map<String, String> metadataMap = convertMetadataToMap(metadata);
+            String itemType = metadataMap.get(".tag");
+
+            if(itemType.equals(ITEM_TYPE_FILE)){
+                String _directoryPath = directoryPath;
+                CompletableFuture
+                    .supplyAsync(() -> {
+                        try {
+                            DbxDownloader<FileMetadata> downloader = files.download(_directoryPath);
+                            FileOutputStream fileOutputStream = new FileOutputStream(downloadDirectoryPath + "/" + metadata.getName());
+                            downloader.download(fileOutputStream);
+                            fileOutputStream.close();
+
+                            return true;
+                        } catch (Exception exception){
+                            exception.printStackTrace();
+                            return false;
+                        }
+                    })
+                    .thenAccept((isDownloaded) -> {
+                        if(isDownloaded){
+                            Long size = Long.parseLong(metadataMap.get("size"));
+                            callback.onDownloadComplete(
+                                new Response.OnDownloadCompleteResponse("", getJobID(), downloadDirectoryPath, metadata.getName(), size)
+                            );
+                        }
+                    });
+            } else if(itemType.equals(ITEM_TYPE_FOLDER)){
+                directory.makeDir(downloadDirectoryPath + "/" + metadata.getName());
+                callback.onDownloadComplete(
+                    new Response.OnDownloadCompleteResponse("", getJobID(), downloadDirectoryPath, metadata.getName(), 0L)
+                );
+
+                String _directoryPath = directoryPath;
+                CompletableFuture
+                    .runAsync(() -> {
+                        try {
+                            ListFolderResult listFolderResult = files.listFolder(_directoryPath);
+                            while(true){
+                                listFolderResult
+                                    .getEntries()
+                                    .forEach(subDirectory -> {
+                                        this.download(subDirectory.getPathLower(), downloadDirectoryPath + "/" + metadata.getName(), callback);
+                                    });
+
+                                if(listFolderResult.getHasMore()){
+                                    listFolderResult = files.listFolderContinue(listFolderResult.getCursor());
+                                } else {
+                                    break;
+                                }
+                            }
+                        } catch (Exception exception){
+                            exception.printStackTrace();
+                        }
+                    });
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean upload(String localDirectoryID, String directoryPath, String directoryName, String uploadDirectoryPath, Transfer.Callback callback) {
+        return false;
+    }
+
+    @Override
+    protected void fetchTotalSizeInBytesAndTotalItemsCount(String directoryID, Map output) {
+
+    }
+}
